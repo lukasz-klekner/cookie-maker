6d1b502063d7cc89c84ff8772843d096941575f1
Łukasz Klekner <lukaszklekner@gmail.com>
2022-11-27 22:30:44 +0100

    feat: create home page

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies')
 
 const homeRouter = express.Router()
 
-homeRouter.get('/', (req, res) => res.render('home/index'))
+homeRouter.get('/', (req, res) => res.render('home/index', {
+    cookie: {
+        base: 'light',
+        addons: ['coconut', 'sprinkles', 'honey']
+    },
+    bases: Object.entries(COOKIE_BASES),
+    addons: Object.entries(COOKIE_ADDONS),
+
+}))
 
 module.exports = {
     homeRouter

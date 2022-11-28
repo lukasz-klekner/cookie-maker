7b044311d14bd55b3e7a8ce696b0edf44b46579c
Łukasz Klekner <lukaszklekner@gmail.com>
2022-11-28 08:26:31 +0100

    feat: display parts of cookie and sum

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies')
+const { handlebarsHelpers } = require('../handlebars-helpers')
 
 const homeRouter = express.Router()
 
-homeRouter.get('/', (req, res) => res.render('home/index', {
+homeRouter.get('/', (req, res) => {
+    const sum = handlebarsHelpers['find-price'](Object.entries(COOKIE_BASES), 'light') +
+    ['coconut', 'sprinkles', 'honey'].reduce((prev, current) => prev + handlebarsHelpers['find-price'](Object.entries(COOKIE_ADDONS) ,current), 0)
+
+    res.render('home/index', {
     cookie: {
         base: 'light',
         addons: ['coconut', 'sprinkles', 'honey']
     },
     bases: Object.entries(COOKIE_BASES),
     addons: Object.entries(COOKIE_ADDONS),
+    sum,
 
-}))
+})})
 
 module.exports = {
     homeRouter

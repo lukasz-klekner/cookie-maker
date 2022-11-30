73e11344b44a7c1c8a52831fb0335e39cb51b426
Łukasz Klekner <lukaszklekner@gmail.com>
2022-11-30 09:00:37 +0100

    fix: add some part to order and fix addons partial

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,8 @@ const { handlebarsHelpers } = require('../utils/handlebars-helpers')
 
 const orderRouter = express.Router()
 
-orderRouter.get('/summary', (req, res) => {
+orderRouter
+    .get('/summary', (req, res) => {
     const { cookieBase } = req.cookies
     const addons = getAddonsFromRequest(req)
 
@@ -19,7 +20,15 @@ orderRouter.get('/summary', (req, res) => {
     bases: Object.entries(COOKIE_BASES),
     addons: Object.entries(COOKIE_ADDONS),
     sum,
+})})
+    .get('/thanks', (req, res) => {
+    const { cookieBase } = req.cookies
+    const addons = getAddonsFromRequest(req)
 
+    const sum = (cookieBase ? handlebarsHelpers['find-price'](Object.entries(COOKIE_BASES), cookieBase) : 0) + addons.reduce((prev, current) => prev + handlebarsHelpers['find-price'](Object.entries(COOKIE_ADDONS) ,current), 0)
+
+    res.clearCookie('cookieBase').clearCookie('cookieAddons').render('order/thanks', {
+    sum,
 })})
 
 module.exports = {
